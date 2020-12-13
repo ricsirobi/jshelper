1796d3a4fe780c03fb1842a717ac18d154a350b9
ricsirobi <49874054+ricsirobi@users.noreply.github.com>
2020-12-13 14:23:38 +0100

    Create databaseConnect.js

---
+++ databaseConnect.js
@@ -0,0 +1,21 @@
+function connect(var server, var catalog, var user, var password)
+{
+var connection = new ActiveXObject("ADODB.Connection") ;
+var connectionstring="Data Source=<"+server+">;Initial Catalog=<"+catalog+">;User ID=<"+user+">;Password=<"+password+">;Provider=SQLOLEDB";
+
+connection.Open(connectionstring);
+var rs = new ActiveXObject("ADODB.Recordset");
+
+rs.Open("SELECT * FROM table", connection);
+rs.MoveFirst
+while(!rs.eof)
+{
+   document.write(rs.fields(1));
+   rs.movenext;
+}
+
+rs.close;
+connection.close; 
+
+
+}
